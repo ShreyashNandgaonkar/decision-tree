320c667259a84147779056075d4e39ea3cf0fa90
ShreyashNandgaonkar <46418251+ShreyashNandgaonkar@users.noreply.github.com>
2019-11-29 22:55:54 +0530

    Add files via upload

---
+++ Decision tree_regression_classification.py
@@ -0,0 +1,50 @@
+#Decision tree for classification
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+dataset = pd.read_csv("D:/Datasets/bill_authentication.csv")
+#Data analysis
+dataset.shape
+dataset.head()
+X = dataset.drop('Class', axis=1)
+y = dataset['Class']
+#split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+#training
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+#prediction
+y_pred = classifier.predict(X_test)
+#Evaluation
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+#Decision tree for regression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+dataset = pd.read_csv('D:\Datasets\petrol_consumption.csv')
+dataset.head()
+dataset.describe()
+X = dataset.drop('Petrol_Consumption', axis=1)
+y = dataset['Petrol_Consumption']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+df=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})
+df
+from sklearn import metrics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
